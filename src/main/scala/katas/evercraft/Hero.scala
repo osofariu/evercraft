d1db574cabbe5edab74e08fa925f4f5bfbf24f9a
Ovi Sofariu <ovi@sofariu.com>
2016-01-01 12:35:31 -0500

    using case class for Hero seems to make things simpler

--- src/main/scala/katas/evercraft/Hero.scala
+++ src/main/scala/katas/evercraft/Hero.scala
@@ -2,23 +2,10 @@ package katas.evercraft
 
 import katas.evercraft.Alignment.Neutral
 
-class Hero private (val name: String, val hitPoints: Int, val alignment: Alignment, val experiencePoints : Int,
-                    val strength: Ability, val dexterity : Ability, val constitution : Ability) {
+case class Hero (name: String = "Agnes", hitPoints: Int = 5, alignment: Alignment = Neutral, experiencePoints : Int = 0,
+                         strength: Ability = Ability(), dexterity : Ability = Ability(), constitution : Ability = Ability()) {
   def armorClass = 10
   def wisdom = Ability()
   def intelligence = Ability()
   def charisma = Ability()
-
-  def copy(name: String = name, hitPoints : Int = hitPoints, alignment: Alignment = alignment, experiencePoints : Int = experiencePoints,
-           strength: Ability = strength, dexterity : Ability = dexterity) = {
-    new Hero(name, hitPoints,alignment, experiencePoints, strength, dexterity, constitution)
-  }
-}
-
-object Hero {
-  def apply(name: String ="Agnes", hitPoints: Int = 5, alignment: Alignment = Neutral, experiencePoints : Int = 0,
-            strength: Ability = Ability(), dexterity: Ability = Ability(), constitution: Ability = Ability()): Hero = {
-    new Hero(name = name, hitPoints = hitPoints, alignment = alignment, experiencePoints = experiencePoints,
-      strength = strength, dexterity = dexterity, constitution = constitution)
-  }
 }
